cb389c22965ac73b6a786cefc85fa48f748f2a98
Eng You Guan <youguan@gmail.com>
2025-10-05 20:53:09 +0800

    feat(security): implement Content Security Policy

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,59 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      connect-src 'self' ${
+        process.env.NEXT_PUBLIC_API_URL === '/api'
+          ? ''
+          : process.env.NEXT_PUBLIC_API_URL
+      };
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim();
+
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
